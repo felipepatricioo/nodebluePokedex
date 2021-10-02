009dd84335a6fea5c10ab53db99a083129b5fd24
Felipe Patrício <86684357+felipepatricioo@users.noreply.github.com>
2021-10-01 21:07:34 -0300

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
